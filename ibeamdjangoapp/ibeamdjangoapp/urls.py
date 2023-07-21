cd47fbd36c5c1113b24cd0734c988b60e6021e51
alexj <alexj@LAPTOP-7048P6HN>
2023-07-22 07:43:42 +1000

    configured app to print hello world to the screen

--- ibeamdjangoapp/ibeamdjangoapp/urls.py
+++ ibeamdjangoapp/ibeamdjangoapp/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from ibeamdjangoapp.views import hello_world
+
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('hello/', hello_world, name='hello_world'),
 ]
