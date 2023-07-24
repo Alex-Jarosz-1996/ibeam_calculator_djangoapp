4aa9c70688c5ddd5cf7f5fcd4ba1c72096f5e0be
alexj <alexj@LAPTOP-7048P6HN>
2023-07-24 21:42:45 +1000

    configured post request

--- ibeamroot/ibeamapp/views.py
+++ ibeamroot/ibeamapp/views.py
@@ -1,5 +1,28 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from ibeamapp.utils import ibeam
    
+
 def get_ibeam_form(request):
-    return render(request, 'base.html')
+    if request.method == 'POST':
+        sigma = float(request.POST['sigma'])
+        force_applied = float(request.POST['force_applied'])
+        beam_thick = float(request.POST['beam_thick'])
+        num_distances = int(request.POST['num_distances'])
+        distances = [float(request.POST[f'distance{i}']) \
+                     for i in range(1, num_distances+1)]
+        
+        # using the ibeam() function to create the height, width
+        # relationships
+        ibeam_dict = ibeam(sigma, 
+                           force_applied, 
+                           beam_thick, 
+                           distances)
+        
+        context = {'sigma': sigma, 'force_applied': force_applied,
+                   'beam_thick': beam_thick, 'results': ibeam_dict}
+
+        # Render the same template again
+        return render(request, 'distance.html', context)
+        
+    else:
+        return render(request, 'base.html')
